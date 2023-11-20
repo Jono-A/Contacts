bd71b9be109a14753513e6a803b8f5a538f7a3d0
Jono Alcantara <202101054@iacademy.edu.ph>
2023-11-20 18:01:21 +0800

    renaming

--- app/src/main/java/com/example/petadoptionfinals/ui/AddPetActivity.kt
+++ app/src/main/java/com/example/petadoptionfinals/ui/AddPetActivity.kt
@@ -1,6 +1,8 @@
 package com.example.petadoptionfinals.ui
 
+import android.app.ProgressDialog
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -9,14 +11,19 @@ import com.example.petadoptionfinals.databinding.ToolbarTitleBinding
 import com.example.petadoptionfinals.model.petModel
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import java.io.File
 import java.io.FileWriter
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class AddPetActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityAddPetBinding
     private lateinit var toolbarBinding : ToolbarTitleBinding
     private lateinit var database : DatabaseReference
+    private lateinit var ImageUri : Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +38,37 @@ class AddPetActivity : AppCompatActivity() {
                 writeData()
                 startActivity(Intent(this@AddPetActivity, MainActivity::class.java))
             }
+
         }
+
+        binding.addImage.setOnClickListener{
+
+            selectImage()
+        }
+
+
+
+    }
+
+
+
+
+    private fun selectImage() {
+
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, 100)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 100 && resultCode == RESULT_OK){
+            ImageUri = data?.data!!
+            binding.firebaseImage.setImageURI(ImageUri)
+        }
+
     }
 
 
@@ -55,6 +92,28 @@ class AddPetActivity : AppCompatActivity() {
     }
 
     private fun addContact(): Boolean {
+        val progressDialog = ProgressDialog (this)
+        progressDialog.setMessage("Uploading Pet Image...")
+        progressDialog.setCancelable(false)
+        progressDialog.show()
+
+        val formatter = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.getDefault())
+        val now = Date()
+        val fileName = formatter.format(now)
+        val storageReference = FirebaseStorage.getInstance().getReference("images/$fileName")
+
+        storageReference.putFile(ImageUri).
+        addOnSuccessListener {
+            binding.firebaseImage.setImageURI(null)
+            Toast.makeText(this@AddPetActivity, "Successfully uploaded", Toast.LENGTH_SHORT).show()
+            if (progressDialog.isShowing) progressDialog.dismiss()
+        }.addOnFailureListener{
+
+            if (progressDialog.isShowing)progressDialog.dismiss()
+            Toast.makeText(this@AddPetActivity,"Failed",Toast.LENGTH_SHORT).show()
+        }
+
+
         val name = binding.inName.text.toString()
         val email = binding.inEmail.text.toString()
         val phone = binding.inPhone.text.toString()
@@ -70,11 +129,13 @@ class AddPetActivity : AppCompatActivity() {
             Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
         }
 
+
         if (!inputRequirements(name, email, phone)) {
             return false
         }
 
         return true
+
     }
 
     private fun writeData() {
@@ -90,4 +151,5 @@ class AddPetActivity : AppCompatActivity() {
             stream.close()
         }
     }
+
 }
